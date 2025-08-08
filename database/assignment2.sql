e3ef2d493e2f3ea52aa872844a52db13a622e052
desivar <jillian_desire@yahoo.es>
2025-08-07 21:06:28 -0600

    modify assignment2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,34 +1,44 @@
--- Insert Record for `Tony Stark` into table `account`
-INSERT INTO public.account(account_firstname, account_lastname, account_email, account_password)
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+-- Insert Tony Stark Record in 'account'
+INSERT INTO account (
+	account_firstname,
+	account_lastname,
+	account_email,
+	account_password
+) VALUES (
+	'Tony',
+	'Stark',
+	'tony@starknet.com',
+	'Iam1ronM@n'
+)
 
--- Update Record for `Tony Stark` account_type to be `Admin`
-UPDATE public.account 
+-- Update Tony Stark account type --
+UPDATE account
 SET account_type = 'Admin'
-WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+WHERE account_id = 1;
 
--- Delete Record for `Tony Stark` from table `account`
-DELETE FROM public.account
-WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+-- Delete Tony Stark record --
+DELETE FROM account 
+WHERE account_id = 1;
 
--- Modify Record `GM Hummer` to update `small interiors` to be `a huge interior`
-UPDATE public.inventory
-SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
-WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+-- Update / Replace text for GM Hummer --
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 
+							 'the small interiors', -- added "the" since it would have read "gives you the a huge interior" --
+							 'a huge interior')
+WHERE inv_id = 10;
 
--- Using inner join to select the make and model fields from table `inventory` 
--- and the classification name field from the table `classification` 
--- for inventory items that belong to the `Sport` classification. 
-SELECT i.inv_make, i.inv_model 
-FROM inventory i 
-INNER JOIN classification c 
-ON i.classification_id = c.classification_id
-WHERE classification_name = 'Sport';
+-- Inner Join inventory / classification --
+SELECT inv_make, inv_model, cla.classification_name
+FROM inventory inv
+LEFT JOIN classification cla
+    ON inv.classification_id = cla.classification_id
+WHERE cla.classification_name = 'Sport';
 
--- Update all Records in table `inventory` to add '/vehicles' to the middle of 
--- the file path in the inv_image and inv_thumbnail columns. 
--- e.g., /images/vehicles/a-car-name.jpg
-UPDATE public.inventory
-SET 
-    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+-- Update / Replace file path for images & thumbnails --
+UPDATE inventory
+SET inv_image = REPLACE(inv_image,
+						'images/',
+						'images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail,
+						'images/',
+						'images/vehicles/'); 
